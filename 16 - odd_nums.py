be2b936bd7b04245256fa98941446d858e450bd7
Jacob Dowdy <jacobdowdy@rocketmail.com>
2019-05-19 01:33:46 -0400

    Create 16 - odd_nums.py

---
+++ 16 - odd_nums.py
@@ -0,0 +1,8 @@
+# [ ] create a list of odd numbers (odd_nums) from 1 to 25 using range(start,stop,skip)
+# [ ] print odd_nums
+# hint: odd numbers are 2 digits apart
+odd_nums = []
+for num in range(1,26,2):
+    odd_nums.append(num)
+
+print(odd_nums)
