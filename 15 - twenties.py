c4cb220fc9deaaad362edfb855d7afd8eface224
Jacob Dowdy <jacobdowdy@rocketmail.com>
2019-05-19 01:29:59 -0400

    Create 15 - twenties.py

---
+++ 15 - twenties.py
@@ -0,0 +1,9 @@
+# [ ] build a list of numbers from 20 to 29: twenties 
+# append each number to twenties list using range(start,stop) iteration
+# [ ] print twenties
+twenties = []
+
+for number in range(20,30):
+    twenties.append(number)
+    
+print(twenties)
