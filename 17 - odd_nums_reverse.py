3d7b3af86697b87580b5aeb8f4ceb522e5b46b32
Jacob Dowdy <jacobdowdy@rocketmail.com>
2019-05-19 01:35:42 -0400

    Create 17 - odd_nums_reverse.py

---
+++ 17 - odd_nums_reverse.py
@@ -0,0 +1,7 @@
+# [ ] create a Decending list of odd numbers (odd_nums) from 25 to 1 using range(start,stop,skip)
+# [ ] print odd_nums,  output should resemble [25, 23, ...]
+odd_nums = []
+for num in range(25,0,-2):
+    odd_nums.append(num)
+
+print(odd_nums)
